a51b23f630fecb4a52b000e20d242612750f5e19
ELIAV <eliav9896@gmail.com>
2020-04-17 00:24:01 +0300

    Exercise 4 and 5. Main diff: 1. The code now talks in one language of 'state' (before: 'position' and 'state'), all places adjusted accordingly. 2. Added the implementation of temporal differencing algorithm 3. Added the implementation of SARSA algorithm

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Apr  9 12:34:36 2020
-Originsl code have been taken from https://github.com/kumararduino/Reinforcement-learning/blob/master/Q-Learning/git_simple_game.py
+Original code have been taken from https://github.com/kumararduino/Reinforcement-learning/blob/master/Q-Learning/git_simple_game.py
 @author: AntonD
 """
-
+import seaborn as sns
 import numpy as np
 import pygame
 # import matplotlib.pylab as pl
@@ -13,18 +13,19 @@ from agent import Agent
 
 alpha = 0.01
 gamma = 0.9
-current_pos = [0, 0]
+current_pos = 0
 epsilon = 0.25
+NUM_OF_EPISODES = 1000000
 
 
-def testBellman():
-    'Calculates and shows Bellmans Eq.'
+def test_bellman():
+    """Calculates and shows Bellman Eq."""
     board1 = GameBoard()
     agent = Agent(board1, alpha, gamma, current_pos, epsilon)
-    A, B, V = agent.BelamanEq()
+    A, B, V = agent.bellman_eq()
 
     V = V.reshape((board1.n, board1.n))
-    board1.drawboard_withnum(V)
+    board1.draw_board_with_num(V)
     print("V Matrix")
     for i in range(V.shape[0]):
         line = ""
@@ -32,25 +33,12 @@ def testBellman():
             line += ' %5.3f ' % (V[i, j])
         print(line)
 
-        # print ("A Matrix")
-    # for i in range(A.shape[0]):
-    #     line = ""
-    #     for j in range(A.shape[1]):
-    #         line += ' %5.3f ' % (A[i,j])
-    #     print (line) 
-    # line = ""
-    # for i in range(len(B)):
-    #     line += ' %5.3f ' % (B[i])
-    #     print (B[i])               
-    # print ('B = ')
-    # print (line)          
 
-
-def testValueIteration():
+def test_value_iteration():
     board1 = GameBoard()
     agent = Agent(board1, alpha, gamma, current_pos, epsilon)
     V, steps = agent.value_iteration()
-    A, B, v = agent.BelamanEq()
+    A, B, v = agent.bellman_eq()
     delta = V - v
     delta = delta.reshape((board1.n, board1.n))
     V = V.reshape((board1.n, board1.n))
@@ -60,39 +48,58 @@ def testValueIteration():
             line += ' %5.3f ' % (V[i, j])
         print(line)
     print('step number = ', steps)
-    board1.drawboard_withnum(V)
+    board1.draw_board_with_num(V)
 
 
-def testPolicy_iteration():
+def test_policy_iteration():
     board1 = GameBoard()
     agent = Agent(board1, alpha, gamma, current_pos, epsilon)
     policy, V, steps = agent.policy_iteration()
     V = V.reshape((board1.n, board1.n))
-    board1.drawboard_withnum(V)
+    board1.draw_board_with_num(V)
     print(policy)
     print(steps)
 
     return policy
 
 
-def test4():
-    pass
+def test_tmp_difference(num_of_episodes):
+    board1 = GameBoard()
+    agent = Agent(board1, alpha, gamma, current_pos, epsilon)
+    V = agent.tmp_difference(num_of_episodes)
+    V = V.reshape((board1.n, board1.n))
+    board1.draw_board_with_num(V)
+    pygame.image.save(board1.screen, "stage_4-TD.jpg")
+    for i in range(V.shape[0]):
+        line = ""
+        for j in range(V.shape[1]):
+            line += ' %5.3f ' % (V[i, j])
+        print(line)
 
 
-testBellman()
-# testValueIteration()
-# tmp = testPolicy_iteration()
+def test_sarsa(num_of_episodes):
+    board1 = GameBoard()
+    agent = Agent(board1, alpha, gamma, current_pos, epsilon)
+    q, init_q = agent.sarsa(num_of_episodes)
+    print(q)
+    print(init_q)
+
+
+"""
+    # saving the tables as sns 'heatmap' with annotations
+    sns.set()
+    q_plot = sns.heatmap(Q, annot=True, fmt='.2f')
+    figure = q_plot.get_figure()
+    figure.savefig('Final_Q_table.png', dpi=400)
+    init_q_plot = sns.heatmap(init_Q, annot=True, fmt='.2f')
+    figure = init_q_plot.get_figure()
+    figure.savefig('Initial_Q_table.png', dpi=400)
+"""
+
+# test_bellman()
+# test_value_iteration()
+# test_policy_iteration()
+test_tmp_difference(NUM_OF_EPISODES)
+# test_sarsa(NUM_OF_EPISODES)
 input()
 pygame.quit()
-
-# run = True
-# while run:
-#     # sleep(0.3)
-#     screen.fill(background)
-#     layout()
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             run = False
-#     pygame.display.flip()
-#     episode()
-# pygame.quit()
