b9200a1d72cb9e0125a76f8ceddb50605d7d96f1
ELIAV <eliav9896@gmail.com>
2020-05-12 19:15:24 +0300

    Exercise 6: 1. Q-learning implementation (agent.py) 2. Q(lambda) implementation (agent.py) 3. Test functions for each algorithm (main.py) 3. Summary and graphs (main.py)

--- agent.py
+++ agent.py
@@ -4,6 +4,7 @@ Created on Thu Apr  9 12:34:36 2020
 Original code have been taken from https://github.com/kumararduino/Reinforcement-learning/blob/master/Q-Learning/git_simple_game.py
 @author: AntonD
 """
+import math
 
 import numpy as np
 from random import randint as r
@@ -11,23 +12,35 @@ import random
 from gameboard import GameBoard
 
 
+def init_q(env):
+    q = np.random.random((env.n ** 2, 4))
+    for i in range(len(q)):
+        for j in [0, 1, 2, 3]:
+            actions = [k[0] for k in env.get_actions(i)]
+            if j not in actions:
+                q[i, j] = -np.power(10, 4)
+
+    return q
+
+
 class Agent:
-    def __init__(self, env: GameBoard, alpha=0.01, gamma=0.9, current_state=0, epsilon=0.25):
+    def __init__(self, env: GameBoard, alpha=0.01, lamda=0.9, gamma=0.9, current_state=0, epsilon=0.1):
         """Agent that can use different tactic and algorithms playing on static board/environment
             The goal is to find the the end point as fast as possible at unknown environments.
             The environment can be characterized by Markov Decision Process
             This class have to get environment class or GameBoard"""
         # policy = None
         self.alpha = alpha
+        self.lamda = lamda
         self.gamma = gamma
-        self.current_state = current_state or 0  # current pos set to 0 in case its it's None
+        self.current_state = current_state
         self.epsilon = epsilon
         self.env = env
         self.policy = None
 
-        # initialize Q
         # self.Q = np.zeros((env.n ** 2, 4))  # Initializing Q-Table
-        self.Q = np.random.random((env.n ** 2, 4))  # Initializing Q-Table
+        self.Q = init_q(env)  # Initializing Q-Table
+        # self.Q = np.random.random((env.n ** 2, 4)) * 0.05 + np.ones((env.n ** 2, 4)) * 0.475  # Initializing Q-Table
 
     # -----------  Task 2  -----------
     def bellman_eq(self):
@@ -145,7 +158,7 @@ class Agent:
 
     # -----------  Task 4  -----------
     def tmp_difference(self, num_episodes):
-        V = np.zeros(self.env.n ** 2)  # Initialize the value of all states to 0
+        v = np.zeros(self.env.n ** 2)  # Initialize the value of all states to 0
         # loop over all episodes
         action = [0, 0]
         for episode in range(num_episodes):
@@ -158,13 +171,12 @@ class Agent:
                 # choose a random action from the current state
                 action = random.choice(self.env.get_actions(self.current_state))
                 # update the Value of the state
-                V[self.current_state] = V[self.current_state] + self.alpha * (
-                        self.env.get_reward(self.current_state) + self.gamma * V[action[1]] - V[self.current_state])
-        return V
+                v[self.current_state] = v[self.current_state] + self.alpha * (
+                        self.env.get_reward(self.current_state) + self.gamma * v[action[1]] - v[self.current_state])
+        return v
 
     # -----------  Task 5  -----------
     def sarsa(self, num_episodes):
-        self.policy, V, steps = self.policy_iteration()
         initial_q = np.copy(self.Q)
         self.epsilon = 0.1
         for episode in range(num_episodes):
@@ -172,34 +184,168 @@ class Agent:
             while self.current_state not in self.env.terminals:
                 # change to next state only after we enter the while for
                 self.current_state = next_state
-                action, next_state = self.greedy_action()  # (action, next state)
-                next_action, next_next_state = (self.greedy_action(next_state))  # (next state action, next next state)
+                action, next_state = self.epsilon_greedy_action()  # (action, next state)
+                next_action, next_next_state = (self.epsilon_greedy_action(next_state))  # (next state action, next next state)
                 reward = self.env.get_reward(self.current_state)
                 self.Q[self.current_state, action] = self.Q[self.current_state, action] + self.alpha * (
                         reward + self.gamma * self.Q[next_state][next_action] - self.Q[self.current_state, action])
 
         return self.Q, initial_q
 
-    def greedy_action(self, state=None):
+    def get_greedy_policy(self, q_table):
+        actions_dict = {0: -7, 1: 7, 2: -1, 3: 1}
+        policy_array = []
+        state = 0
+        for i in range(17):
+            policy_array.append(state)
+            lst_temp = [action[0] for action in self.env.get_actions(state)]
+            chosen_action = random.choice(np.where(q_table[state] == max(q_table[state][lst_temp]))[0])
+            state += actions_dict[int(chosen_action)]
+        return policy_array
+
+        # -----------  Task 6  -----------
+
+    def q_learning(self, num_episodes):
+        episodes = np.array([])
+        count = 0
+        cum_reward = 0
+        for episode in range(num_episodes):
+
+            self.current_state = next_state = 0
+            while self.current_state not in self.env.terminals:
+                count = count + 1
+                action, next_state = self.epsilon_greedy_action()  # take next action from epsilon-greedy policy (action, next state)
+                next_action, next_next_state = self.max_q_value(next_state)  # act greedly
+                # get reward from taking an action at current_sate
+                reward = self.env.get_reward(self.current_state)
+                cum_reward = cum_reward + reward
+
+                # update q value by TD rule
+                self.Q[self.current_state, action] = self.Q[self.current_state, action] + self.alpha * (
+                        reward + self.gamma * self.Q[next_state][next_action] - self.Q[self.current_state, action])
+                # change to next state only after we update the q values
+                self.current_state = next_state
+
+            count = count + 1
+            # when current_State is terminal - update for the final time then start a new episode
+            action, next_state = self.epsilon_greedy_action()
+            next_action, next_next_state = self.max_q_value(next_state)
+            reward = self.env.get_reward(self.current_state)
+            cum_reward = cum_reward + reward
+
+            self.Q[self.current_state, action] = self.Q[self.current_state, action] + self.alpha * (
+                    reward + self.gamma * self.Q[next_state][next_action] - self.Q[self.current_state, action])
+
+            episodes = np.append(episodes, self.Q[1][1])
+        #            if self.current_state==48:
+        #                episodes=np.append(episodes, cum_reward/count)
+        #                episodes=np.append(episodes, cum_reward)
+        #                episodes=np.append(episodes, count)
+        #                count=0
+        #                cum_reward=0
+
+        return episodes
+
+    def max_q_value(self, state=None):
+        state = state
+        """choose action based on greedy policy"""
+        if state == None:
+            state = self.current_state
+
+        if state in self.env.terminals:
+            # in case we are on terminal, just take a valid action (all lead to same reward) and set next state as 0
+            return random.choice([action[0] for action in self.env.get_actions(state)]), 0
+        else:
+            # list of all possible actions
+            lst_temp = [action[0] for action in self.env.get_actions(state)]
+
+            # make a list of possible action to pick from
+            choose_max_from = [self.Q[state][i] for i in lst_temp]
+
+            # pick the max q value of possible actions
+            chosen_action = random.choice(np.where(self.Q[state] == max(choose_max_from))[0])
+            # return tuple: (action index , next state)
+            return chosen_action, self.env.get_actions(state)[lst_temp.index(chosen_action)][1]
+
+    def epsilon_greedy_action(self, state=None):
+        state = state
         """choose action based on epsilon-greedy policy"""
-        state = state or self.current_state
+        if state == None:
+            state = self.current_state
+
         if state in self.env.terminals:
             # in case we are on terminal, just took a valid action (all lead to same reward) and set next state as 0
             return random.choice([action[0] for action in self.env.get_actions(state)]), 0
         else:
             if random.random() > self.epsilon:
-                # list of all possible actions
-                lst_temp = [action[0] for action in self.env.get_actions(state)]
-                # in case of same max-q-value between actions, pick randomly between them
-                chosen_action = random.choice(np.where(self.policy[state] == max(self.policy[state]))[0])
-                # return tuple: (action index , next state)
-                return chosen_action, self.env.get_actions(state)[lst_temp.index(chosen_action)][1]
+                return self.max_q_value(state)
             else:
                 return random.choice(self.env.get_actions(state))
 
-    # -----------  Task 6  -----------
-    def q_learning(self):
-        pass
+    def extract_greedy_policy(self):
+        p = []
+        state = 0
+        while state not in self.env.terminals:
+            p.append(state)
+            state = self.max_q_value(state)[1]
+        p.append(state)
+        return p
 
-    def eligibility_traces(self):
-        pass
+    def q_lamda(self, num_episodes):
+        episodes = np.array([])
+        count = 0
+        cum_reward = 0
+        for episode in range(num_episodes):
+            self.current_state = next_state = 0
+            self.traces = np.zeros((self.env.n ** 2, 4))
+
+            while self.current_state not in self.env.terminals:
+                count = count + 1
+
+                action, next_state = self.epsilon_greedy_action()  # take next action from epsilon-greedy policy (action, next state)
+                next_possible_action, next_next_state1 = self.epsilon_greedy_action(next_state)
+                next_action, next_next_state2 = self.max_q_value(next_state)  # act greedly
+                # get reward from taking an action at current_sate
+                reward = self.env.get_reward(self.current_state)
+
+                cum_reward = cum_reward + reward
+                # update the TD error (matrix -wise)
+                TD_error = reward + self.gamma * self.Q[next_state][next_action] - self.Q[self.current_state, action]
+
+                # update the current state's trace by 1
+                self.traces[self.current_state][action] = self.traces[self.current_state][action] + 1
+                # update q value by TD rule
+                self.Q = self.Q + self.alpha * (TD_error * self.traces)
+
+                if next_action == next_possible_action:
+                    self.traces = self.traces * self.gamma * self.lamda
+
+                else:
+                    self.traces = np.zeros((self.env.n ** 2, 4))
+
+                self.current_state = next_state
+
+            count = count + 1
+            # when current_State is terminal - update for the final time then start a new episode
+            action, next_state = self.epsilon_greedy_action()
+            next_possible_action, next_next_state1 = self.epsilon_greedy_action(next_state)
+            next_action, next_next_state2 = self.max_q_value(next_state)
+            reward = self.env.get_reward(self.current_state)
+
+            cum_reward = cum_reward + reward
+            TD_error = reward + self.gamma * self.Q[next_state][next_action] - self.Q[self.current_state, action]
+            self.traces[self.current_state][action] = self.traces[self.current_state][action] + 1
+            self.Q = self.Q + self.alpha * (TD_error * self.traces)
+
+            episodes = np.append(episodes, self.Q[1][1])
+        #            if self.current_state==48:
+
+        #                episodes=np.append(episodes, cum_reward/count)
+        #                episodes=np.append(episodes, cum_reward)
+        #                episodes=np.append(episodes, count)
+        #                count=0
+        #                cum_reward=0
+
+        #            self.epsilon =1/(episode+1)
+
+        return episodes
--- main.py
+++ main.py
@@ -7,15 +7,24 @@ Original code have been taken from https://github.com/kumararduino/Reinforcement
 import seaborn as sns
 import numpy as np
 import pygame
-# import matplotlib.pylab as pl
 from gameboard import GameBoard
 from agent import Agent
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.lines import Line2D
+import random
+from operator import itemgetter
 
-alpha = 0.01
+alpha = 0.001
 gamma = 0.9
+lamda = 0.9
 current_pos = 0
 epsilon = 0.25
-NUM_OF_EPISODES = 1000000
+NUM_OF_EPISODES = 100000
+
+# both are important!
+np.random.seed(679)
+random.seed(679)
 
 
 def test_bellman():
@@ -66,29 +75,60 @@ def test_policy_iteration():
 def test_tmp_difference(num_of_episodes):
     board1 = GameBoard()
     agent = Agent(board1, alpha, gamma, current_pos, epsilon)
-    V = agent.tmp_difference(num_of_episodes)
+    V, V_count = agent.tmp_difference(num_of_episodes)
     V = V.reshape((board1.n, board1.n))
     board1.draw_board_with_num(V)
-    pygame.image.save(board1.screen, "stage_4-TD.jpg")
-    for i in range(V.shape[0]):
-        line = ""
-        for j in range(V.shape[1]):
-            line += ' %5.3f ' % (V[i, j])
-        print(line)
+    pygame.image.save(board1.screen, "temporal_differencing_results/stage_4-TD_10e6_values.jpg")
+    V_count = V_count.reshape((board1.n, board1.n))
+    board1.draw_board_with_num(V_count)
+    pygame.image.save(board1.screen, "temporal_differencing_results/stage_4-TD_10e6_count.jpg")
 
 
 def test_sarsa(num_of_episodes):
     board1 = GameBoard()
     agent = Agent(board1, alpha, gamma, current_pos, epsilon)
-    q, init_q = agent.sarsa(num_of_episodes)
-    print(q)
-    print(init_q)
+    q, init_q, greedy_policy, first_optimal_episode = agent.sarsa(num_of_episodes)
+    print("the greedy policy: " + str(greedy_policy) + ", been discovered first at the: " + str(first_optimal_episode) + " episode")
+
+
+def test_q_learning(num_of_episodes):
+    board1 = GameBoard()
+    agent = Agent(board1, alpha, lamda, gamma, current_pos, epsilon)
+    array = np.copy(agent.q_learning(num_of_episodes))
+    return array
+
+
+def test_q_lamda(num_of_episodes):
+    board1 = GameBoard()
+    agent = Agent(board1, alpha, lamda, gamma, current_pos, epsilon)
+    array = np.copy(agent.q_lamda(num_of_episodes))
+    return array
+
+
+def test_by_plot(array1, array2):
+    a = min(len(array1), len(array2))
+    array1 = array1[0:a]
+    array2 = array2[0:a]
+    df = pd.DataFrame(array1, columns=["Q-Learning"])
+    df["Q-Lambda"] = array2
+    df = df.reset_index()
+    df = df.rename(columns={'index': 'episodes'})
+    df = df.melt('episodes', var_name='algorithms', value_name='Q[1]-down')
+    ax = sns.lineplot(x="episodes", y='Q[1]-down', hue='algorithms', data=df)
+    return df
 
 
 # test_bellman()
 # test_value_iteration()
 # test_policy_iteration()
 # test_tmp_difference(NUM_OF_EPISODES)
-test_sarsa(NUM_OF_EPISODES)
-input()
+# test_sarsa(NUM_OF_EPISODES)
+# test_q_learning(NUM_OF_EPISODES)
+
+array1 = test_q_learning(NUM_OF_EPISODES)
 pygame.quit()
+
+array2 = test_q_lamda(NUM_OF_EPISODES)
+pygame.quit()
+
+test_by_plot(array1, array2)
